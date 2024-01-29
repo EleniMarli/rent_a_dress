a05eee5a8ad2949067eeb2d7296d9b162e362137
Eleni Marli <elenimarli@yahoo.gr>
2024-01-29 16:39:07 +0100

    Integrate User model with devise and add name attribute with a migration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Default url options for devise
+  config.action_mailer.default_url_options = { host: 'rent-a-dress-cdc99ed9126a.herokuapp.com', port: 3000 }
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
