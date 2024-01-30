3d550381e6ae201f83e47da7a92d6898d0e730f9
Eva Ungerer <ungerer.eva@gmail.com>
2024-01-30 17:21:29 +0100

    solved the syntax error of the has many relation in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :dresses, dependent: :destroy
   has_many :bookings, dependent: :destroy
-  has_many :booked_dresses, through :bookings, class_name :dress
+  has_many :booked_dresses, through: :bookings, class_name: 'Dress'
 
   # validates :email, presence: true, uniqueness: true
   # validates :password, presence: true, length: { minimum: 6 }
