489316a5799591095c2b5d428722b2fbf32e8d33
Eleni Marli <147054119+EleniMarli@users.noreply.github.com>
2024-01-30 16:21:34 +0100

    Update dress.rb
    Delete enum for category and size and add inclusion validations for both

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -2,13 +2,13 @@ class Dress < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
 
-  validates :title, presence: true, length: { minimum: 10 }
+  validates :title, presence: true, length: { minimum: 6 }
   validates :description, presence: true, length: { minimum: 20 }
   validates :size, presence: true
   validates :color, presence: true
   validates :category, presence: true
   validates :price_per_day, presence: true, numericality: { greater_than: 0 }
 
-  enum category: { casual: 'casual', formal: 'formal', cocktail: 'cocktail', maxi: 'maxi', sundress: 'sundress', evening: 'evening', party: 'party', summer:'summer'}
-  enum size: { s: 's', m: 'm', l: 'l', xl: 'xl', xxl: 'xxl' }
+  validates_inclusion_of :category, in: %w(casual formal cocktail maxi sundress evening party summer)
+  validates_inclusion_of :size, in: %w(s m l xl xxl)
 end
