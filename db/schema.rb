2fbf9a6da590bda5adb839d964d14cb48b193b1c
Ruth Iruoaboroghene Felix <ruthflex@yahoo.co.uk>
2024-01-30 14:48:16 +0100

    created Bookings Model and role validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_29_160349) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_30_113301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "dress_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["dress_id"], name: "index_bookings_on_dress_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "dresses", force: :cascade do |t|
     t.string "title"
@@ -40,5 +52,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_29_160349) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "dresses"
+  add_foreign_key "bookings", "users"
   add_foreign_key "dresses", "users"
 end
